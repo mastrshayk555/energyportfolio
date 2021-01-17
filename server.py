e393e5da1757705b5fe7fbef8a24db9c87669d10
mastrshayk555 <jeffreyheim55@gmail.com>
2021-01-16 21:08:31 -0500

    Add Contacts and Projects Refactor

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, send_from_directory, url_for, request, redirect
+import passcheck
 import csv
 
 app = Flask(__name__)
@@ -10,47 +11,50 @@ def my_home():
     return render_template('index.html')
 
 
-#@app.route('/<string:page_name>')
-#def html_page(page_name):
-#    return render_template(page_name)
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-'''
+@app.route('/check_password', methods=['POST', 'GET'])
+def check_password():
+    if request.method == 'POST':
+        try:
+            password = request.form['password']
+            count = passcheck.pwned_api_check(password)
+        except:
+            return 'something failed'
+        if count:
+            return f'{password} was found {count} times... You should probably change your password.'
+        else:
+            return f"{password} was NOT found. OK to proceed."
+    else:
+        return 'Something did not work'
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-	if request.method == 'POST':
-		try:
-			data = request.form.to_dict()
-			write_to_csv(data)
-			return redirect('/thankyou.html')
-		except:
-			return 'Did not save to database'
-	else:
-		return 'Something went wrong. Try again.'
-'''
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Did not save to database'
+    else:
+        return 'Something went wrong. Try again.'
 
-'''
-def write_to_file(data):
-	with open('database.txt', mode='a') as database:
-		email = data['email']
-		subject = data['subject']
-		message = data['message']
-		file = database.write(f'\n{email},{subject},{message}')
-'''
 
-'''
 def write_to_csv(data):
-	with open('database.csv', newline='', mode='a') as database2:
-		email = data['email']
-		subject = data['subject']
-		message = data['message']
-		csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		csv_writer.writerow([email,subject,message])
-'''
+    with open('./contacts/contacts.csv', newline='', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
-'''
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-'''
+
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
